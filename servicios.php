867527c2e8f7702ef7253ab92dfdd4e8dd81b5cc
Nayely2020-40166 <barrientosmaya69@gmail.com>
2021-08-26 09:15:15 -0600

    se hizo la conexion a php

---
+++ servicios.php
@@ -0,0 +1,219 @@
+
+<html lang="es">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>SERVICIOS/PRODUCTOS</title>
+  </head>
+  <body>
+
+  <div class="btn-group w-100" role="group" aria-label="Basic example">
+      <a class="btn btn-outline-warning" href="servicios.php" role="button">Servicios</a>
+      <a class="btn btn-outline-info" href="tienda.php" role="button">Tienda</a>
+      <a class="btn btn-outline-danger" href="contactenos.php" role="button">Contáctenos</a>
+    </div>
+
+
+
+
+    <br><br><br>
+    <div class="container"><h3>Productos/Servicios</h3></div>
+    <br><br>
+    
+    <div class="container">
+  <div class="row">
+  <div class="col-md-3 col-sm-6">
+  <div class="card" style="width: 17rem;">
+  <img src="imagenes/cap3.jpg" height="210px" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>
+
+
+  <div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div> 
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div> 
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+</div>
+</div>
+<br><br><br>
+<div class="container">
+  <div class="row">
+  <div class="col-md-3 col-sm-6">
+  <div class="card" style="width: 17rem;">
+  <img src="imagenes/cap2.jpg" height="210px" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>
+
+
+  <div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div> 
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+</div>
+</div>
+<br><br><br>
+<div class="container">
+  <div class="row">
+  <div class="col-md-3 col-sm-6">
+  <div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>
+
+
+  <div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div> 
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div> 
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="card" style="width: 17rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+</div>
+</div>
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<br><br><br><br><br><br><br><br>
+<table class="table table-success table-striped">
+  <thead>
+    <tr>
+      <th scope="col">PBX:123456789   DIRECCIÓN: 5 CALLE ZONA 12 MIXCO</th>
+      <th scope="col">CARNÉ: 2020-40166</th>
+      <th scope="col">NOMBRE: NAYELY BANESSA BARRIENTOS DE LEÓN</th>
+    </tr>
+  </thead>
+</table>
+
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
---
+++ tienda.php
@@ -0,0 +1,242 @@
+
+<html lang="es">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>SERVICIOS/PRODUCTOS</title>
+  </head>
+  <body>
+
+  <div class="btn-group w-100" role="group" aria-label="Basic example">
+      <a class="btn btn-outline-warning" href="servicios.php" role="button">Servicios</a>
+      <a class="btn btn-outline-info" href="tienda.php" role="button">Tienda</a>
+      <a class="btn btn-outline-danger" href="contactenos.php" role="button">Contáctenos</a>
+    </div>
+
+
+    <div class="col-md-3 col-sm-6">
+
+    <br><br><br>
+    <div class="container"><h3>Productos/Servicios</h3></div>
+    <br><br>
+    <center>
+  <div class="container">
+  <div class="row align-items-start">
+    <div class="col">
+        <img src="image/sueter1.jpg" style="width: 50%;">
+      </div>
+    <div class="col">
+        <img src="image/sueter2.jpg" style="width: 50%;">
+</div>
+    <div class="col">
+        <img src="image/sueter3.jpg" style="width: 50%;">
+      </div>
+      <div class="col">
+        <img src="image/sueter3.jpg" style="width: 50%;">
+      </div>
+  </div>
+  <br>
+  <div class="row align-items-center">
+    <div class="col">
+       Sueter Rosado
+      </div>
+    <div class="col">
+      Sueter Gris Grande
+    </div>
+    <div class="col">
+      Sueter Verde Con Tirantes
+    </div>
+    <div class="col">
+      Sueter Verde Con Tirantes
+    </div>
+
+    <br>
+  </div>
+  <div class="row align-items-end">
+    <div class="col">
+        <b>Precio: Q150.00</b>
+        <br><br>
+        <button class="btn btn-info" type="submit">Más Información</button>
+       </div>
+    <div class="col">
+     <b>Precio: Q50.20</b>
+     <br><br>
+     <button class="btn btn-info" type="submit">Más Información</button>
+    </div>
+    <div class="col">
+      <b>Precio: Q140.12</b>
+      <br><br>
+      <button class="btn btn-info" type="submit">Más Información</button>
+    </div>
+    <div class="col">
+      <b>Precio: Q140.12</b>
+      <br><br>
+      <button class="btn btn-info" type="submit">Más Información</button>
+    </div>
+  </div>
+</div>
+<br><br>
+
+
+
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container">
+    <div class="row align-items-start">
+      <div class="col">
+          <img src="image/sueter4.jpg" style="width: 50%;">
+        </div>
+      <div class="col">
+          <img src="image/sueter5.jpg" style="width: 50%;">
+  </div>
+      <div class="col">
+          <img src="image/sueter6.jpg" style="width: 50%;">
+        </div>
+        <div class="col">
+          <img src="image/sueter6.jpg" style="width: 50%;">
+        </div>
+    </div>
+    <br>
+    <div class="row align-items-center">
+      <div class="col">
+        Sueter Rojo Con Tirantes
+        </div>
+      <div class="col">
+        Sueter Gris Con Cuello Alto
+      </div>
+      <div class="col">
+        Sueter Gris Largo
+      </div>
+      <div class="col">
+        Sueter Gris Largo
+      </div>
+      <br>
+    </div>
+    <div class="row align-items-end">
+      <div class="col">
+          <b>Precio: Q200.20</b>
+          <br><br>
+          <button class="btn btn-info" type="submit">Más Información</button>
+         </div>
+      <div class="col">
+       <b>Precio: Q212.00</b>
+       <br><br>
+       <button class="btn btn-info" type="submit">Más Información</button>
+      </div>
+      <div class="col">
+        <b>Precio: Q547.00</b>
+        <br><br>
+        <button class="btn btn-info" type="submit">Más Información</button>
+      </div>
+      <div class="col">
+        <b>Precio: Q547.00</b>
+        <br><br>
+        <button class="btn btn-info" type="submit">Más Información</button>
+      </div>
+    </div>
+  </div>
+  <br><br>
+  <div class="container">
+    <div class="row align-items-start">
+      <div class="col">
+          <img src="image/buso1.jpg" style="width: 50%;">
+        </div>
+      <div class="col">
+          <img src="image/buso2.jpg" style="width: 50%;">
+  </div>
+      <div class="col">
+          <img src="image/buso3.jpg" style="width: 50%;">
+        </div>
+        <div class="col">
+          <img src="image/buso3.jpg" style="width: 50%;">
+        </div>
+    </div>
+    <br>
+    <div class="row align-items-center">
+      <div class="col">
+         Buso Pequeño 
+        </div>
+      <div class="col">
+        Buso Negro
+      </div>
+      <div class="col">
+        Buso Pequeño Con Flores En Las Mangas
+      </div>
+      <div class="col">
+        Buso Pequeño Con Flores En Las Mangas
+      </div>
+      <br>
+    </div>
+    <div class="row align-items-end">
+      <div class="col">
+          <b>Precio: Q145.21</b>
+          <br><br>
+          <button class="btn btn-info" type="submit">Más Información</button>
+         </div>
+      <div class="col">
+       <b>Precio: Q41.20</b>
+       <br><br>
+       <button class="btn btn-info" type="submit">Más Información</button>
+      </div>
+      <div class="col">
+        <b>Precio: Q180.00</b>
+        <br><br>
+        <button class="btn btn-info" type="submit">Más Información</button>
+      </div>
+      <div class="col">
+        <b>Precio: Q180.00</b>
+        <br><br>
+        <button class="btn btn-info" type="submit">Más Información</button>
+      </div>
+    </div>
+  </div>
+  
+</center>
+
+<br><br><br><br><br><br><br><br>
+<table class="table table-success table-striped">
+  <thead>
+    <tr>
+      <th scope="col">PBX:123456789   DIRECCIÓN: 5 CALLE ZONA 12 MIXCO</th>
+      <th scope="col">CARNÉ: 2020-40166</th>
+      <th scope="col">NOMBRE: NAYELY BANESSA BARRIENTOS DE LEÓN</th>
+    </tr>
+  </thead>
+</table>
+
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
