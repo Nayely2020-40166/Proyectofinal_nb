7e30fc79b80989dc9cc5a0b0ce7ea120539797eb
Nayely2020-40166 <barrientosmaya69@gmail.com>
2021-08-26 13:37:50 -0600

    Se actualizo

---
+++ contactenos.php
@@ -0,0 +1,67 @@
+
+<html lang="es">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>CONTACTECNOS</title>
+  </head>
+  <body>
+
+  <div class="btn-group w-100" role="group" aria-label="Basic example">
+      <a class="btn btn-outline-warning" href="index.html" role="button">Inicio</a>
+      <a class="btn btn-outline-info" href="tienda.php" role="button">Tienda</a>
+      <a class="btn btn-outline-danger" href="contactenos.php" role="button">Contáctenos</a>
+      <a class="btn btn-outline-dark" href="iniciar.php" role="button">Ver Mensajes</a>
+    </div>
+
+    <br><br><br>
+    <div class="container"><h3>Contáctenos</h3></div>
+    <br><br>
+  
+
+    <div class="container">
+<h5>NOMBRES</h5>
+<input class="form-control" type="text" placeholder="Ingrese nombre y apellido" aria-label="default input example">
+<br><br>
+<h5>TELEFONO / CELULAR</h5>
+<input class="form-control" type="text" placeholder="Ingrese numero telefonico, debe contar con 8 digitos" aria-label="default input example">
+<br><br>
+<h5>CORREO ELECTRONICO</h5>
+<input class="form-control" type="text" placeholder="Ingrese correo electronico" aria-label="default input example">
+<br><br>
+<h5>MENSAJE</h5>
+<input class="form-control" type="text" placeholder="Ingrese su mensaje" aria-label="default input example">
+<br><br>
+<center>
+    <input type="submit" class="btn btn-info" value="Enviar Mensaje" name="enviar"> 
+</center>
+    </div>
+   
+
+
+
+
+
+
+<br><br><br>
+<table class="table table-info table-striped">
+  <thead>
+    <tr>
+      <th scope="col">PBX:123456789   DIRECCIÓN: 5 CALLE ZONA 12 MIXCO</th>
+      <th scope="col">CARNÉ: 2020-40166</th>
+      <th scope="col">NOMBRE: NAYELY BANESSA BARRIENTOS DE LEÓN</th>
+    </tr>
+  </thead>
+</table>
+
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
